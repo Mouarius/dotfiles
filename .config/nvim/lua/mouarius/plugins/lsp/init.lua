20aa7615ad00140e1aa33de82397a477df36b6d0
Marius Menault <mouarius@gmail.com>
2023-10-23 22:39:54 +0200

    first lsp config

--- .config/nvim/lua/mouarius/plugins/lsp/init.lua
+++ .config/nvim/lua/mouarius/plugins/lsp/init.lua
@@ -1,10 +1,36 @@
 return {
   {'williamboman/mason.nvim'},
   {'williamboman/mason-lspconfig.nvim'},
-  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
+    config = function()
+      local lsp_zero = require("lsp-zero")
+      lsp_zero.setup()
+
+      lsp_zero.on_attach(function(client, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      local lspconfig = require("lspconfig")
+
+      lspconfig.lua_ls.setup({})
+
+      require("mason").setup({})
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "tsserver",
+          "html",
+          "cssls",
+          "jedi_language_server"
+        },
+        handlers = {
+          lsp_zero.default_setup,
+        }
+      })
+    end
+  },
   {'neovim/nvim-lspconfig'},
   {'hrsh7th/cmp-nvim-lsp'},
   {'hrsh7th/nvim-cmp'},
   {'L3MON4D3/LuaSnip'},
 }
-require"mouarius.plugins.lsp.config"
