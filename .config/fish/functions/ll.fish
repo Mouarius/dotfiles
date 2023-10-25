583e8b796d42e79fab96332aad37e1579efd8c36
Marius Menault <marius.me@hellowatt.fr>
2023-10-25 10:19:42 +0200

    remove unused

--- .config/fish/functions/ll.fish
+++
@@ -1,3 +0,0 @@
-function ll
-	exa -g -l --icons $argv
-end
--- .config/fish/functions/sv.fish
+++
@@ -1,8 +0,0 @@
-function sv
-    if test $argv
-        source ~/src/venv/$argv/bin/activate.fish
-        if test $status != 0
-            echo "Unable to find the virtual environment $argv"
-        end
-    end
-end
