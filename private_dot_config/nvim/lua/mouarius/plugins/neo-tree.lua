1a60092ff0bde2dac2f725eb99aa7982335ea310
Marius Menault <marius.me@hellowatt.fr>
2023-12-01 22:56:53 +0100

    update config

--- private_dot_config/nvim/lua/mouarius/plugins/neo-tree.lua
+++ private_dot_config/nvim/lua/mouarius/plugins/neo-tree.lua
@@ -1,54 +1,100 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	cmd = "Neotree",
-	keys = {
-		{
-			"<C-n>",
-			function()
-				require("neo-tree.command").execute({ toggle = true, reveal = true })
-			end,
-			desc = "Explorer NeoTree (root dir)",
-		},
-		{
-			"<C-N>",
-			function()
-				require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-			end,
-			desc = "Explorer NeoTree (cwd)",
-		},
-		{
-			"<leader>ge",
-			function()
-				require("neo-tree.command").execute({ source = "git_status", toggle = true })
-			end,
-			desc = "Git explorer",
-		},
-		{
-			"<leader>be",
-			function()
-				require("neo-tree.command").execute({ source = "buffers", toggle = true })
-			end,
-			desc = "Buffer explorer",
-		},
-	},
-	deactivate = function()
-		vim.cmd([[Neotree close]])
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	opts = {
-		window = {
-			filesystem = {
-				bind_to_cwd = false,
-				follow_current_file = { enabled = true },
-				use_libuv_file_watcher = true,
-				hijack_netrw_behavior = "open_default",
-			},
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  cmd = "Neotree",
+  keys = {
+    {
+      "<C-n>",
+      function()
+        require("neo-tree.command").execute({ toggle = true, reveal = true })
+      end,
+      desc = "Explorer NeoTree",
+    },
+    {
+      "<leader>eg",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", toggle = true })
+      end,
+      desc = "Git explorer",
+    },
+    {
+      "<leader>eb",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", toggle = true })
+      end,
+      desc = "Buffer explorer",
+    },
+  },
+  deactivate = function()
+    vim.cmd([[Neotree close]])
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    {
+      "s1n7ax/nvim-window-picker",
+      event = "VeryLazy",
+      version = "2.*",
+      opts = {
+        highlights = {
+          statusline = {
+            focused = {
+              fg = "#f9e2af",
+              bg = "#313244",
+              bold = true,
+            },
+            unfocused = {
+              fg = "#cdd6f4",
+              bg = "#313244",
+              bold = false,
+            },
+          },
+        },
+      },
+    },
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  opts = {
+    window = {
+      filesystem = {
+        bind_to_cwd = false,
+        follow_current_file = { enabled = true },
+        use_libuv_file_watcher = true,
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = false, -- when true, they will just be displayed differently than normal items
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          ".DS_Store",
+          "__pycache__"
+        },
+      },
+      window = {
+        mapping_options = {
+          noremap = true,
+          nowait = false,
+        },
+        mappings = {
+          ["o"] = { "open", nowait = true },
+          -- ["oc"] = { command = "none" },
+          -- ["od"] = { command = "none" },
+          -- ["og"] = { command = "none" },
+          -- ["om"] = { command = "none" },
+          -- ["on"] = { command = "none" },
+          -- ["os"] = { command = "none" },
+          -- ["ot"] = { command = "none" },
+          ["O"] = { command = "show_help" },
+          ["Oc"] = { command = "order_by_created" },
+          ["Od"] = { command = "order_by_diagnostics" },
+          ["Og"] = { command = "order_by_git_status" },
+          ["Om"] = { command = "order_by_modified" },
+          ["On"] = { command = "order_by_name" },
+          ["Os"] = { command = "order_by_size" },
+          ["Ot"] = { command = "order_by_type" },
+        },
+      },
+    },
+  },
 }
