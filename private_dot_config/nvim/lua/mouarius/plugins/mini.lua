1904cbcc018c93d733b720576f5cbe27edba0d52
Marius Menault <marius.me@hellowatt.fr>
2024-01-07 20:07:21 +0100

    add todo highlights

--- private_dot_config/nvim/lua/mouarius/plugins/mini.lua
+++ private_dot_config/nvim/lua/mouarius/plugins/mini.lua
@@ -1,26 +1,26 @@
 return {
-  {
-    "echasnovski/mini.bufremove",
-    version = false,
-    keys = {
-      {
-        "<leader>bd",
-        function()
-          local bd = require("mini.bufremove").delete
-          if vim.bo.modified then
-            local choice = vim.fn.confirm("Save changes to %q?", "&Yes\n&No\n&Cancel", "Warning")
-            if choice == 1 then
-              vim.cmd.write()
-              bd(0)
-            elseif choice == 2 then
-              bd(0, true)
-            end
-          else
-            bd(0)
-          end
-        end,
-        desc = "Delete buffer",
-      },
-    },
-  },
+	{
+		"echasnovski/mini.bufremove",
+		version = false,
+		keys = {
+			{
+				"<leader>bd",
+				function()
+					local bd = require("mini.bufremove").delete
+					if vim.bo.modified then
+						local choice = vim.fn.confirm("Save changes to %q?", "&Yes\n&No\n&Cancel", "Warning")
+						if choice == 1 then
+							vim.cmd.write()
+							bd(0)
+						elseif choice == 2 then
+							bd(0, true)
+						end
+					else
+						bd(0)
+					end
+				end,
+				desc = "Delete buffer",
+			},
+		},
+	},
 }
