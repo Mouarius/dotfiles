505a8dc6f9a6122e04e4e874f6654797aace6961
Marius Menault <mouarius@gmail.com>
2024-03-17 16:39:47 +0100

    add font

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 13.0
+config.font = wezterm.font "JetBrains Mono"
 config.color_scheme = "Catppuccin Mocha"
 -- WEZTERM $TERM information
 -- see : https://wezfurlong.org/wezterm/config/lua/config/term.html?h=term
