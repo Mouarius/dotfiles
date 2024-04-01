9a427171e0e28dfc3401254cf50696d0a034fd4f
Marius Menault <mouarius@gmail.com>
2024-04-01 16:29:03 +0200

    unmanage nvim

--- private_dot_config/nvim/test-file.lua
+++
@@ -1,8 +0,0 @@
-local env_name = vim.fn.getenv("VIRTUAL_ENV")
-local EXPECTED_VIRTUAL_ENV = "/home/mariusmenault/dev/venv/hw"
-local file_path = vim.api.nvim_buf_get_name(0)
-local is_in_mysite = string.find(file_path, "^/home/mariusmenault/dev/greenday/mysite/") == 1
-
-if env_name == EXPECTED_VIRTUAL_ENV and is_in_mysite then
-  
-end
