8643ef0dd482b52b3d93c7c5aa4ac4923d75e0c9
Mouarius <mouarius@gmail.com>
2025-02-13 19:52:15 +0100

    Updage config adding aliases

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -40,10 +40,17 @@ alias gkb "g checkout -b"
 alias glog "g log --graph --oneline --decorate"
 alias gm "g merge"
 alias lg "lazygit"
+
+# AWS
 alias sveb "source /Users/mariusmenault/.ebcli-virtual-env/bin/activate.fish"
+alias ebweb "cd ~/dev/eb/France-Staging-web/ && eb ssh --profile=prod"
+alias ebwor "cd ~/dev/eb/France-Staging-worker/ && eb ssh --profile=prod"
 
 # django
 alias pm "python manage.py"
+alias mm "python manage.py makemigrations --name"
+alias mmm "python manage.py makemigrations --merge"
+
 
 # pre-commit
 alias pcr "pre-commit run"
@@ -55,11 +62,13 @@ alias build "npm run build:production"
 
 command -qv nvim && alias vim nvim
 set -gx EDITOR nvim
+# only for greenday
+set -gx UV_INDEX_GITLAB_REGISTRY_PASSWORD gldt-r-7GyKzpCcmmK9p8tMME
 
 # test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-if command -v ngrok &>/dev/null;
-    eval "$(ngrok completion)"
-end
+# if command -v ngrok &>/dev/null
+#     eval "$(ngrok completion)"
+# end
 
 fzf --fish | source
 zoxide init fish | source
