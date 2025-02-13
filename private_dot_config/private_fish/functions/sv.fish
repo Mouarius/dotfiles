c915a08eb52dde6d288f5d48c758b5880c6fc353
Mouarius <mouarius@gmail.com>
2025-02-13 19:52:07 +0100

    Update sv

--- private_dot_config/private_fish/functions/sv.fish
+++ private_dot_config/private_fish/functions/sv.fish
@@ -8,6 +8,12 @@ function sv
             echo "Unable to find the virtual environment $argv"
         end
     else
-        echo "You need to provide the name of a virtual env in arguments"
+        if test $VIRTUAL_ENV
+            eval "deactivate"
+        end
+        source .venv/bin/activate.fish
+        if test $status != 0
+            echo "Unable to find the virtual environment $argv"
+        end
     end
 end
